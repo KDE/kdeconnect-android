KDE/kdeconnect-android

commit 76c9def95b85cb0e336b89c3f1bdfd2042465f04

Fixed a race condition where onConnectionLost was called before onConnectionAccepted
+ MprisActivity now tries to reconnect to the plugin when there is a new connection,
just in case the new connection replaced the old one (the plugin would have been reloaded)

KdeConnect/src/main/java/org/kde/connect/Device.java
@@ -117,9 +117,11 @@ public class Device implements BaseComputerLink.PackageReceiver {
     //
 
     public void addLink(BaseComputerLink link) {
-        Log.e("Device","addLink "+link.getLinkProvider().getName()+" -> "+getName());
 
         links.add(link);
+
+        Log.e("Device","addLink "+link.getLinkProvider().getName()+" -> "+getName() + " active links: "+ links.size());
+
 
         Collections.sort(links, new Comparator<BaseComputerLink>() {
             @Override
@@ -138,6 +140,7 @@ public class Device implements BaseComputerLink.PackageReceiver {
     public void removeLink(BaseComputerLink link) {
         link.removePackageReceiver(this);
         links.remove(link);
+        Log.e("Device","removeLink: "+link.getLinkProvider().getName() + " -> "+getName() + " active links: "+ links.size());
         if (links.isEmpty()) {
             reloadPluginsFromSettings();
         }

KdeConnect/src/main/java/org/kde/connect/Plugins/NotificationsPlugin.java
@@ -190,11 +190,11 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
             byte[] bitmapData = outStream.toByteArray();
             byte[] serializedBitmapData = Base64.encode(bitmapData, Base64.NO_WRAP);
             String stringBitmapData = new String(serializedBitmapData, Charset.defaultCharset());
-            //Es super gran lol, millor ho fem quan puguem enviar arxius
+            //The icon is super big, better sending it as a file transfer when we support that
             //np.set("base64icon", stringBitmapData);
         } catch(Exception e) {
             e.printStackTrace();
-            Log.e("NotificationsPlugin","Error retrieveing icon");
+            Log.e("NotificationsPlugin","Error retrieving icon");
         }
 
         np.set("id", id.serialize());
@@ -251,33 +251,33 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
 
         if (Build.VERSION.SDK_INT < 18) {
             return new AlertDialog.Builder(baseContext)
-                    .setTitle("Notifications Plugin")
-                    .setMessage("This plugin is not compatible with Android 4.3")
-                    .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
+                .setTitle("Notifications Plugin")
+                .setMessage("This plugin is not compatible with Android 4.3")
+                .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                        }
-                    })
-                    .create();
+                    }
+                })
+                .create();
         } else {
             return new AlertDialog.Builder(baseContext)
-                    .setTitle("Notifications Plugin")
-                    .setMessage("You need to grant permission to access notifications")
-                    .setPositiveButton("Open settings",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-                            baseContext.startActivity(intent);
-                        }
-                    })
-                    .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            //Do nothing
-                        }
-                    })
-                    .create();
+                .setTitle("Notifications Plugin")
+                .setMessage("You need to grant permission to access notifications")
+                .setPositiveButton("Open settings",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                        baseContext.startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Do nothing
+                    }
+                })
+                .create();
         }
     }
 

KdeConnect/src/main/java/org/kde/connect/BackgroundService.java
@@ -58,38 +58,37 @@ public class BackgroundService extends Service {
         @Override
         public void onConnectionReceived(final NetworkPackage identityPackage, final BaseComputerLink link) {
 
-            Log.i("BackgroundService", "Connection accepted!");
+            Log.e("BackgroundService", "Connection accepted!");
 
-            runOnMainThread(new Runnable() { //Some plugins that create Handlers will crash if started from a different thread!
-                @Override
-                public void run() {
+            new Throwable().printStackTrace();
 
-                    String deviceId = identityPackage.getString("deviceId");
-                    String name = identityPackage.getString("deviceName");
+            String deviceId = identityPackage.getString("deviceId");
 
-                    if (devices.containsKey(deviceId)) {
-                        Log.i("BackgroundService", "known device");
-                        Device device = devices.get(deviceId);
-                        if (!device.hasName()) device.setName(identityPackage.getString("deviceName"));
-                        device.addLink(link);
-                    } else {
-                        Log.i("BackgroundService", "unknown device");
-                        Device device = new Device(getBaseContext(), deviceId, name, link);
-                        devices.put(deviceId, device);
-                    }
-                }
-            });
+            Device device = devices.get(deviceId);
 
+            if (device != null) {
+                Log.e("BackgroundService", "addLink, known device: "+deviceId);
+                if (!device.hasName()) device.setName(identityPackage.getString("deviceName"));
+                device.addLink(link);
+            } else {
+                Log.e("BackgroundService", "addLink,unknown device: "+deviceId);
+                String name = identityPackage.getString("deviceName");
+                device = new Device(getBaseContext(), deviceId, name, link);
+                devices.put(deviceId, device);
+            }
         }
 
         @Override
         public void onConnectionLost(BaseComputerLink link) {
             Device d = devices.get(link.getDeviceId());
+            Log.e("onConnectionLost","removeLink, deviceId: "+link.getDeviceId());
             if (d != null) {
                 d.removeLink(link);
                 if (!d.isReachable() && !d.isTrusted()) {
                     devices.remove(link.getDeviceId());
                 }
+            } else {
+                Log.e("onConnectionLost","Removing connection to unknown device, this should not happen");
             }
 
         }
@@ -187,12 +186,6 @@ public class BackgroundService extends Service {
         if (callback != null) callbacks.add(callback);
         Intent serviceIntent = new Intent(c, BackgroundService.class);
         c.startService(serviceIntent);
-    }
-
-
-    Handler mainHandler = new Handler(Looper.getMainLooper());
-    private void runOnMainThread(Runnable runnable) {
-        mainHandler.post(runnable);
     }
 
 }

KdeConnect/src/main/java/org/kde/connect/LinkProviders/BroadcastTcpLinkProvider.java
@@ -48,6 +48,7 @@ public class BroadcastTcpLinkProvider extends BaseLinkProvider {
                 String deviceId = brokenLink.getDeviceId();
                 if (visibleComputers.get(deviceId) == brokenLink) {
                     visibleComputers.remove(deviceId);
+                    connectionLost(brokenLink);
                 }
             }
 
@@ -158,8 +159,8 @@ public class BroadcastTcpLinkProvider extends BaseLinkProvider {
     };
 
     private void addLink(NetworkPackage identityPackage, NioSessionComputerLink link) {
-        Log.e("BroadcastTcpLinkProvider","addLink to "+identityPackage.getString("deviceName"));
         String deviceId = identityPackage.getString("deviceId");
+        Log.e("BroadcastTcpLinkProvider","addLink to "+deviceId);
         BaseComputerLink oldLink = visibleComputers.get(deviceId);
         if (oldLink != null) {
             Log.e("BroadcastTcpLinkProvider","Removing old connection to same device");

KdeConnect/src/main/java/org/kde/connect/MprisActivity.java
@@ -13,6 +13,8 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.kde.connect.ComputerLinks.BaseComputerLink;
+import org.kde.connect.LinkProviders.BaseLinkProvider;
 import org.kde.connect.Plugins.MprisPlugin;
 import org.kde.kdeconnect.R;
 
@@ -20,23 +22,20 @@ import java.util.ArrayList;
 
 public class MprisActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mpris_control);
+    protected void connectToPlugin() {
 
         final String deviceId = getIntent().getStringExtra("deviceId");
-
 
         BackgroundService.RunCommand(this, new BackgroundService.InstanceCallback() {
             @Override
             public void onServiceStart(BackgroundService service) {
 
+                Log.e("MprisActivity", "onService start");
+
                 Device device = service.getDevice(deviceId);
                 final MprisPlugin mpris = (MprisPlugin) device.getPlugin("plugin_mpris");
                 if (mpris == null) {
-                    Log.e("MprisActivity","device has no mpris plugin!");
-                    //TODO: Show error
+                    Log.e("MprisActivity", "device has no mpris plugin!");
                     return;
                 }
 
@@ -66,6 +65,7 @@ public class MprisActivity extends Activity {
 
                 mpris.setPlayerListUpdatedHandler(new Handler() {
                     boolean firstLoad = true;
+
                     @Override
                     public void handleMessage(Message msg) {
                         final ArrayList<String> playerList = mpris.getPlayerList();
@@ -81,11 +81,14 @@ public class MprisActivity extends Activity {
                                 //String prevPlayer = (String)spinner.getSelectedItem();
                                 spinner.setAdapter(adapter);
 
+                                Log.e("MprisActivity", "playerListUpdatedHandler");
+
                                 spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                                     @Override
                                     public void onItemSelected(AdapterView<?> arg0, View arg1, int pos, long id) {
                                         ((TextView) findViewById(R.id.now_playing_textview)).setText("");
                                         String player = playerList.get(pos);
+                                        Log.e("MprisActivity", "onPlayerSelected: " + player);
                                         mpris.setPlayer(player);
                                         //Spotify doesn't support changing the volume yet...
                                         if (player.equals("Spotify")) {
@@ -113,6 +116,33 @@ public class MprisActivity extends Activity {
 
             }
         });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mpris_control);
+
+        final String deviceId = getIntent().getStringExtra("deviceId");
+
+        BackgroundService.RunCommand(MprisActivity.this, new BackgroundService.InstanceCallback() {
+            @Override
+            public void onServiceStart(BackgroundService service) {
+                service.addConnectionListener(new BaseLinkProvider.ConnectionReceiver() {
+                    @Override
+                    public void onConnectionReceived(NetworkPackage identityPackage, BaseComputerLink link) {
+                        connectToPlugin();
+                    }
+
+                    @Override
+                    public void onConnectionLost(BaseComputerLink link) {
+
+                    }
+                });
+            }
+        });
+        connectToPlugin();
 
         findViewById(R.id.play_button).setOnClickListener(new View.OnClickListener() {
             @Override

KdeConnect/src/main/java/org/kde/connect/Plugins/ClipboardPlugin.java
@@ -92,6 +92,7 @@ public class ClipboardPlugin extends Plugin {
 
     @Override
     public boolean onPackageReceived(NetworkPackage np) {
+
         if (!np.getType().equals(NetworkPackage.PACKAGE_TYPE_CLIPBOARD)) {
             return false;
         }

commit b52d4470930b5baa44c50166f40f0cb063e05eb9

Fully working notifications sync
Changed notifications sync protocol
Fixed unserialization of NotificationId
Fixed a bug on Device.addPlugin introduced in commit 76c9def95b85cb0e336b89c3f1bdfd2042465f04, because onCreate was not executed in the main thread

KdeConnect/src/main/java/org/kde/connect/Device.java
@@ -3,6 +3,8 @@ package org.kde.connect;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
@@ -178,39 +180,45 @@ public class Device implements BaseComputerLink.PackageReceiver {
         return plugins.get(name);
     }
 
-    private Plugin addPlugin(String name) {
+    private void addPlugin(final String name) {
         Plugin existing = plugins.get(name);
         if (existing != null) {
             Log.e("addPlugin","plugin already present:" + name);
-            return existing;
+            return;
         }
 
-        Plugin plugin = PluginFactory.instantiatePluginForDevice(context, name, this);
+        final Plugin plugin = PluginFactory.instantiatePluginForDevice(context, name, this);
         if (plugin == null) {
-            Log.e("addPlugin","could not create plugin: "+name);
+            Log.e("addPlugin","could not instantiate plugin: "+name);
             failedPlugins.put(name, plugin);
-            return null;
+            return;
         }
 
-        try {
-            boolean success = plugin.onCreate();
-            if (!success) {
-                failedPlugins.put(name, plugin);
-                return null;
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    boolean success = plugin.onCreate();
+                    if (!success) {
+                        Log.e("addPlugin", "plugin failed to load " + name);
+                        failedPlugins.put(name, plugin);
+                        return;
+                    }
+                } catch (Exception e) {
+                    failedPlugins.put(name, plugin);
+                    e.printStackTrace();
+                    Log.e("addPlugin", "Exception loading plugin " + name);
+                    return;
+                }
+
+                //Log.e("addPlugin","added " + name);
+
+                failedPlugins.remove(name);
+                plugins.put(name, plugin);
             }
-        } catch (Exception e) {
-            failedPlugins.put(name, plugin);
-            Log.e("addPlugin","Exception calling onCreate for "+name);
-            e.printStackTrace();
-            return null;
-        }
+        });
 
-        Log.e("addPlugin",name);
-
-        failedPlugins.remove(name);
-        plugins.put(name, plugin);
-
-        return plugin;
     }
 
     private boolean removePlugin(String name) {
@@ -228,12 +236,12 @@ public class Device implements BaseComputerLink.PackageReceiver {
         try {
             plugin.onDestroy();
         } catch (Exception e) {
-            Log.e("addPlugin","Exception calling onCreate for "+name);
             e.printStackTrace();
+            Log.e("removePlugin","Exception calling onDestroy for plugin "+name);
             return false;
         }
 
-        Log.e("removePlugin",name);
+        //Log.e("removePlugin","removed " + name);
 
         return true;
     }

KdeConnect/src/main/java/org/kde/connect/Plugins/NotificationsPlugin.java
@@ -1,6 +1,5 @@
 package org.kde.connect.Plugins;
 
-import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.content.Context;
@@ -19,15 +18,12 @@ import org.kde.connect.Helpers.AppsHelper;
 import org.kde.connect.Helpers.ImagesHelper;
 import org.kde.connect.NetworkPackage;
 import org.kde.connect.NotificationReceiver;
-import org.kde.connect.PluginFactory;
 import org.kde.kdeconnect.R;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.Charset;
 
 public class NotificationsPlugin extends Plugin implements NotificationReceiver.NotificationListener {
-
-    private NotificationId lastId;
 
     /*static {
         PluginFactory.registerPlugin(NotificationsPlugin.class);
@@ -58,6 +54,9 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
         return true;
     }
 
+
+
+
     static class NotificationId {
         String packageName;
         String tag;
@@ -75,12 +74,19 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
             int first = s.indexOf(':');
             int last = s.lastIndexOf(':');
             nid.packageName = s.substring(0, first);
-            nid.tag = s.substring(first, last);
+            nid.tag = s.substring(first+1, last);
             if (nid.tag.length() == 0) nid.tag = null;
-            nid.id = Integer.parseInt(s.substring(last));
+            String idString = s.substring(last+1);
+            try {
+                nid.id = Integer.parseInt(idString);
+            } catch(Exception e) {
+                nid.id = 0;
+            }
+            Log.e("NotificationId","unserialize: " + nid.packageName+ ", "+nid.tag+ ", "+nid.id);
             return nid;
         }
         public String serialize() {
+            Log.e("NotificationId","serialize: " + packageName+ ", "+tag+ ", "+id);
             String safePackageName = (packageName == null)? "" : packageName;
             String safeTag = (tag == null)? "" : tag;
             return safePackageName+":"+safeTag+":"+id;
@@ -104,8 +110,6 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
 
 
 
-
-    private boolean hasPermissions;
 
     @Override
     public boolean onCreate() {
@@ -155,19 +159,14 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
 
 
 
-
     @Override
     public void onNotificationRemoved(StatusBarNotification statusBarNotification) {
         NotificationId id = NotificationId.fromNotification(statusBarNotification);
 
-        if (!id.equals(lastId)) {
-            NetworkPackage np = new NetworkPackage(NetworkPackage.PACKAGE_TYPE_NOTIFICATION);
-
-            np.set("id", id.serialize());
-            np.set("isCancel", true);
-
-            device.sendPackage(np);
-        }
+        NetworkPackage np = new NetworkPackage(NetworkPackage.PACKAGE_TYPE_NOTIFICATION);
+        np.set("id", id.serialize());
+        np.set("isCancel", true);
+        device.sendPackage(np);
     }
 
     @Override
@@ -211,7 +210,7 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
 
 
     @Override
-    public boolean onPackageReceived(NetworkPackage np) {
+    public boolean onPackageReceived(final NetworkPackage np) {
         if (!np.getType().equals(NetworkPackage.PACKAGE_TYPE_NOTIFICATION)) return false;
 
         if (np.getBoolean("request")) {
@@ -226,13 +225,14 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
                 }
             });
 
-        } else if (np.getBoolean("isCancel")) {
+        } else if (np.has("cancel")) {
 
-            lastId = NotificationId.unserialize(np.getString("id"));
             NotificationReceiver.RunCommand(context, new NotificationReceiver.InstanceCallback() {
                 @Override
                 public void onServiceStart(NotificationReceiver service) {
-                    service.cancelNotification(lastId.getPackageName(), lastId.getTag(), lastId.getId());
+
+                    NotificationId dismissedId = NotificationId.unserialize(np.getString("cancel"));
+                    service.cancelNotification(dismissedId.getPackageName(), dismissedId.getTag(), dismissedId.getId());
                 }
             });
 
@@ -252,7 +252,7 @@ public class NotificationsPlugin extends Plugin implements NotificationReceiver.
         if (Build.VERSION.SDK_INT < 18) {
             return new AlertDialog.Builder(baseContext)
                 .setTitle("Notifications Plugin")
-                .setMessage("This plugin is not compatible with Android 4.3")
+                .setMessage("This plugin is not compatible with Android prior 4.3")
                 .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {

KdeConnect/src/main/java/org/kde/connect/Plugins/PingPlugin.java
@@ -70,7 +70,7 @@ public class PingPlugin extends Plugin {
                     .build();
 
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0, noti);
+            notificationManager.notify((int)System.currentTimeMillis(), noti);
             return true;
 
         }