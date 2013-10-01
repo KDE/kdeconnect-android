58b31fa0326d6748376172b95a5ff5a08d01e511
Albert Astals Cid <aacid@kde.org>
2013-10-01 21:52:09 +0200

    Transition to the StaticMessages.sh thing
    Let's see what next scripty run says
    CCMAIL: albertvaka@gmail.com

--- Messages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir locale
-a2po export --android KdeConnect/src/main/res/ --gettext locale
-mv locale/template.pot $podir/kdeconnect-android.pot
-rm -rf locale
---
+++ StaticMessages.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# The name of catalog we create (without the.pot extension), sourced from the scripty scripts
+FILENAME="kdeconnect-android"
+
+function export_pot_file # First parameter will be the path of the pot file we have to create, includes $FILENAME
+{
+	potfile=$1
+	mkdir outdir
+	a2po export --android KdeConnect/src/main/res/ --gettext outdir
+	mv outdir/template.pot $potfile
+	rm -rf outdir
+}
+
+function import_po_files # First parameter will be a path that will contain several .po files with the format LANG.po
+{
+	podir=$1
+	a2po import --android KdeConnect/src/main/res/ --gettext $podir
+}
+
+
