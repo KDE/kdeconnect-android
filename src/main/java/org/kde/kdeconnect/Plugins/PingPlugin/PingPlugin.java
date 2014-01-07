10210b3972258f8940734e942df9abd6e9157fbd
Samoilenko Yuri <kinnalru@gmail.com>
2014-01-08 01:20:24 +0400

    Fixed Ping notification on old phones
    On old phones(LG P698) notification does not shown ultil all
    fields are filles correctly: setContentIntent.
    Added pendingIntent to notification as describved into official documentation for Notifications, to
    fit support library requirements.

--- src/main/java/org/kde/kdeconnect/Plugins/PingPlugin/PingPlugin.java
+++ src/main/java/org/kde/kdeconnect/Plugins/PingPlugin/PingPlugin.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.View;
 import android.widget.Button;
 
 import org.kde.kdeconnect.NetworkPackage;
 import org.kde.kdeconnect.Plugins.Plugin;
+import org.kde.kdeconnect.UserInterface.MainActivity;
 import org.kde.kdeconnect_tp.R;
 
 
@@ -63,9 +67,18 @@ public class PingPlugin extends Plugin {
         if (np.getType().equals(NetworkPackage.PACKAGE_TYPE_PING)) {
             //Log.e("PingPackageReceiver", "was a ping!");
 
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(new Intent(context, MainActivity.class));
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
             Notification noti = new NotificationCompat.Builder(context)
                     .setContentTitle(device.getName())
                     .setContentText("Ping!")
+                    .setContentIntent(resultPendingIntent)
                     .setTicker("Ping!")
                     .setSmallIcon(android.R.drawable.ic_dialog_alert)
                     .setAutoCancel(true)
