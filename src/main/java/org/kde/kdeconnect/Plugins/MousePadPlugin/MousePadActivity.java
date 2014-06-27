e99ba38eacee14dd54640995b67df8e9816132a0
Albert Vaca <albertvaka@gmail.com>
2014-06-27 17:22:34 +0200

    Removed unnecesary import

--- src/main/java/org/kde/kdeconnect/Plugins/MousePadPlugin/MousePadActivity.java
+++ src/main/java/org/kde/kdeconnect/Plugins/MousePadPlugin/MousePadActivity.java
@@ -2,7 +2,6 @@ package org.kde.kdeconnect.Plugins.MousePadPlugin;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 
