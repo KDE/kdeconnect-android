c837079e03880db69403fa9d174f3e70958a4145
Albert Vaca <albertvaka@gmail.com>
2015-09-09 12:30:24 -0700

    Fixed crash

--- src/org/kde/kdeconnect/UserInterface/PairingFragment.java
+++ src/org/kde/kdeconnect/UserInterface/PairingFragment.java
@@ -158,7 +158,8 @@ public class PairingFragment extends Fragment implements PairingDeviceItem.Callb
 
                             //Restore scroll
                             list.setSelectionFromTop(index, top);
-
+                        } catch (IllegalStateException e) {
+                            //Ignore: The activity was closed while we were trying to update it
                         } finally {
                             listRefreshCalledThisFrame = false;
                         }
