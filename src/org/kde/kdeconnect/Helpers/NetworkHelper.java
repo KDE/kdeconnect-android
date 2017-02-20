b3e1f6fbf86f6432ae6e36ff0a10157a47940af0
Albert Vaca <albertvaka@gmail.com>
2017-02-20 20:25:02 +0100

    Preparing a hotfix release

--- src/org/kde/kdeconnect/Helpers/NetworkHelper.java
+++ src/org/kde/kdeconnect/Helpers/NetworkHelper.java
@@ -8,9 +8,11 @@ import android.util.Log;
 public class NetworkHelper {
 
     public static boolean isOnMobileNetwork(Context context) {
-        final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return false; //This looks a bit dangerous and I prefer not to use it in the next stable release.
+        /*final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info = connMgr.getActiveNetworkInfo();
         return (info != null && info.getType() == ConnectivityManager.TYPE_MOBILE);
+        */
     }
 
 }
