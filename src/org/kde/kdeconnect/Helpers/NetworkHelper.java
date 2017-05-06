9e6a4938ed2f1179bfc7cc4184bc6456555834fa
Albert Vaca <albertvaka@gmail.com>
2017-05-06 12:47:15 +0200

    Added an extra check to be really sure we are on data.

--- src/org/kde/kdeconnect/Helpers/NetworkHelper.java
+++ src/org/kde/kdeconnect/Helpers/NetworkHelper.java
@@ -6,6 +6,9 @@ import android.net.Network;
 import android.net.NetworkInfo;
 import android.util.Log;
 
+import java.io.FileReader;
+import java.io.LineNumberReader;
+
 public class NetworkHelper {
 
     public static boolean isOnMobileNetwork(Context context) {
@@ -27,7 +30,21 @@ public class NetworkHelper {
             Log.e(info.getTypeName(),""+info.isAvailable());
             if (info.isAvailable()) return false; //We are connected to at least one non-mobile network
         }
-        return mobile;
+        if (mobile) { //We suspect we are on a mobile net
+            try {
+                //Check the number of network neighbours, on data it should be 0
+                LineNumberReader is = new LineNumberReader(new FileReader("/proc/net/arp"));
+                is.skip(Long.MAX_VALUE);
+                //Log.e("NetworkHelper", "procnetarp has " + is.getLineNumber() + " lines");
+                if (is.getLineNumber() > 1) { //The first line are the headers
+                    return false; //I have neighbours, so this doesn't look like a mobile network
+                }
+            } catch (Exception e) {
+                Log.e("NetworkHelper", "Exception reading procnetarp");
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 
 }
