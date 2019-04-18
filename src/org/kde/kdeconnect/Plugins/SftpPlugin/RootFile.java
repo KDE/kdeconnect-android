f735520aba71a61333d96e0865947558b3815031
Nicolas Fella <nicolas.fella@kdab.com>
2019-04-19 01:12:06 +0200

    Organize imports

--- src/org/kde/kdeconnect/Plugins/SftpPlugin/RootFile.java
+++ src/org/kde/kdeconnect/Plugins/SftpPlugin/RootFile.java
@@ -22,7 +22,6 @@ package org.kde.kdeconnect.Plugins.SftpPlugin;
 
 import org.apache.sshd.common.file.SshFile;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Calendar;
