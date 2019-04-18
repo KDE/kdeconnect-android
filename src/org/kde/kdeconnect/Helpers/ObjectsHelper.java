794f1d4706835b073ec2726215e6a856c1ba54c5
Nicolas Fella <nicolas.fella@kdab.com>
2019-04-19 00:57:26 +0200

    Remove unneeded file

--- src/org/kde/kdeconnect/Helpers/ObjectsHelper.java
+++
@@ -1,7 +0,0 @@
-package org.kde.kdeconnect.Helpers;
-
-class ObjectsHelper {
-    public static boolean equals(Object a, Object b) {
-        return (a == null) ? (b == null) : a.equals(b);
-    }
-}
