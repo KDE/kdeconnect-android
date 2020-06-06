ee5c75e10aad5c7fb1819dd7b50891e675d08e77
Aditya Mehra <aix.m@outlook.com>
2020-06-06 21:39:41 +0000

    Add support for Bigscreen STT using android's inbuilt speech engine

--- src/org/kde/kdeconnect/Plugins/BigscreenPlugin/BigscreenActivity.java
+++ src/org/kde/kdeconnect/Plugins/BigscreenPlugin/BigscreenActivity.java
@@ -21,15 +21,28 @@
 
 package org.kde.kdeconnect.Plugins.BigscreenPlugin;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
 
 import org.kde.kdeconnect.BackgroundService;
+import org.kde.kdeconnect.UserInterface.MainActivity;
+import org.kde.kdeconnect.UserInterface.PermissionsAlertDialogFragment;
 import org.kde.kdeconnect.UserInterface.ThemeUtil;
 import org.kde.kdeconnect_tp.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class BigscreenActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SPEECH = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +52,57 @@ public class BigscreenActivity extends AppCompatActivity {
 
         final String deviceId = getIntent().getStringExtra("deviceId");
 
-        BackgroundService.RunWithPlugin(this, deviceId, org.kde.kdeconnect.Plugins.BigscreenPlugin.BigscreenPlugin.class, plugin -> runOnUiThread(() -> {
+        if (!SpeechRecognizer.isRecognitionAvailable(this)) {
+            findViewById(R.id.mic_button).setEnabled(false);
+            findViewById(R.id.mic_button).setVisibility(View.INVISIBLE);
+        }
+
+        BackgroundService.RunWithPlugin(this, deviceId, BigscreenPlugin.class, plugin -> runOnUiThread(() -> {
             findViewById(R.id.left_button).setOnClickListener(v -> plugin.sendLeft());
             findViewById(R.id.right_button).setOnClickListener(v -> plugin.sendRight());
             findViewById(R.id.up_button).setOnClickListener(v -> plugin.sendUp());
             findViewById(R.id.down_button).setOnClickListener(v -> plugin.sendDown());
             findViewById(R.id.select_button).setOnClickListener(v -> plugin.sendSelect());
             findViewById(R.id.home_button).setOnClickListener(v -> plugin.sendHome());
+            findViewById(R.id.mic_button).setOnClickListener(v -> {
+                if (plugin.hasMicPermission()) {
+                    activateSTT();
+                } else {
+                    new PermissionsAlertDialogFragment.Builder()
+                            .setTitle(plugin.getDisplayName())
+                            .setMessage(R.string.bigscreen_optional_permission_explanation)
+                            .setPositiveButton(R.string.ok)
+                            .setNegativeButton(R.string.cancel)
+                            .setPermissions(new String[]{Manifest.permission.RECORD_AUDIO})
+                            .setRequestCode(MainActivity.RESULT_NEEDS_RELOAD)
+                            .create().show(getSupportFragmentManager(), null);
+                }
+            });
         }));
+    }
+
+    public void activateSTT() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, R.string.bigscreen_speech_extra_prompt);
+        startActivityForResult(intent, REQUEST_SPEECH);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SPEECH) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                ArrayList<String> result = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (result.get(0) != null) {
+                    final String deviceId = getIntent().getStringExtra("deviceId");
+                    BackgroundService.RunWithPlugin(this, deviceId, BigscreenPlugin.class, plugin -> runOnUiThread(() -> {
+                        plugin.sendSTT(result.get(0));
+                    }));
+                }
+            }
+        }
     }
 }
 
--- src/org/kde/kdeconnect/Plugins/BigscreenPlugin/BigscreenPlugin.java
+++ src/org/kde/kdeconnect/Plugins/BigscreenPlugin/BigscreenPlugin.java
@@ -22,6 +22,7 @@
 package org.kde.kdeconnect.Plugins.BigscreenPlugin;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -41,9 +42,11 @@ import static org.kde.kdeconnect.Plugins.MousePadPlugin.KeyListenerView.SpecialK
 public class BigscreenPlugin extends Plugin {
 
     private final static String PACKET_TYPE_MOUSEPAD_REQUEST = "kdeconnect.mousepad.request";
+    private final static String PACKET_TYPE_BIGSCREEN_STT = "kdeconnect.bigscreen.stt";
 
     @Override
     public boolean onCreate() {
+        optionalPermissionExplanation = R.string.bigscreen_optional_permission_explanation;
         return device.getDeviceType().equals(Device.DeviceType.Tv);
     }
 
@@ -85,17 +88,26 @@ public class BigscreenPlugin extends Plugin {
     }
 
     @Override
-    public String[] getSupportedPacketTypes() {  return new String[0]; }
+    public String[] getSupportedPacketTypes() {  return new String[]{PACKET_TYPE_BIGSCREEN_STT}; }
 
     @Override
     public String[] getOutgoingPacketTypes() {
-        return new String[]{PACKET_TYPE_MOUSEPAD_REQUEST};
+        return new String[]{PACKET_TYPE_MOUSEPAD_REQUEST, PACKET_TYPE_BIGSCREEN_STT};
     }
 
     @Override
     public String getActionName() {
         return context.getString(R.string.pref_plugin_bigscreen);
     }
+
+    public String[] getOptionalPermissions() {
+        return new String[]{Manifest.permission.RECORD_AUDIO};
+    }
+
+    public Boolean hasMicPermission() {
+        return isPermissionGranted(Manifest.permission.RECORD_AUDIO);
+    }
+
 
     public void sendLeft() {
         NetworkPacket np = new NetworkPacket(PACKET_TYPE_MOUSEPAD_REQUEST);
@@ -131,6 +143,13 @@ public class BigscreenPlugin extends Plugin {
         NetworkPacket np = new NetworkPacket(PACKET_TYPE_MOUSEPAD_REQUEST);
         np.set("alt", true);
         np.set("specialKey", SpecialKeysMap.get(KeyEvent.KEYCODE_F4));
+        device.sendPacket(np);
+    }
+
+    public void sendSTT(String content) {
+        NetworkPacket np = new NetworkPacket(PACKET_TYPE_BIGSCREEN_STT);
+        np.set("type", "stt");
+        np.set("content", content);
         device.sendPacket(np);
     }
 }
