ca69a1f4fcf2aca5d16ee4ef7d652d204e2d2f9b
Isira Seneviratne <isirasen96@gmail.com>
2020-08-13 19:33:33 +0530

    Add null check in TelephonyHelper.getAllPhoneNumbers().

--- src/org/kde/kdeconnect/Helpers/TelephonyHelper.java
+++ src/org/kde/kdeconnect/Helpers/TelephonyHelper.java
@@ -46,6 +46,11 @@ public class TelephonyHelper {
             return Collections.emptyList();
         }
         List<SubscriptionInfo> subscriptionInfos = subscriptionManager.getActiveSubscriptionInfoList();
+        if (subscriptionInfos == null) {
+            // This happens when there is no SIM card inserted
+            Log.w(LOGGING_TAG, "Could not get SubscriptionInfos");
+            return Collections.emptyList();
+        }
         List<Integer> subscriptionIDs = new ArrayList<>(subscriptionInfos.size());
         for (SubscriptionInfo info : subscriptionInfos) {
             subscriptionIDs.add(info.getSubscriptionId());
