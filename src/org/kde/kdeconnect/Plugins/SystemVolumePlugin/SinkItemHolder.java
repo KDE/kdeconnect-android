e86f731188ce4cfd2c0973a97c0415bb861f999a
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2021-06-21 10:52:27 +0530

    systemvolumeplugin: make long-press display device details, instead of displaying the label again

--- src/org/kde/kdeconnect/Plugins/SystemVolumePlugin/SinkItemHolder.java
+++ src/org/kde/kdeconnect/Plugins/SystemVolumePlugin/SinkItemHolder.java
@@ -78,7 +78,7 @@ class SinkItemHolder extends RecyclerView.ViewHolder
 
     @Override
     public boolean onLongClick(View v) {
-        Toast.makeText(v.getContext(), sink.getDescription(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(v.getContext(), sink.getName(), Toast.LENGTH_SHORT).show();
         return true;
     }
 
