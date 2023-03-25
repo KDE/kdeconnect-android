b8cbd2b382217f1d4a4fd7a4e5f949f79f290f22
Mihir Patel <mpatel88@gmail.com>
2023-03-25 19:25:39 +0000

    Adding support for per-app language preferences on Android 13 and later
    Android 13 added the ability to select languages on a per-app basis. Supporting this feature simply requires us to provide a listing of all of the locales supported by the app: https://developer.android.com/guide/topics/resources/app-languages
    
    This change adds a locales_config.xml file to list the locales and references this file in the application manifest

--- StaticMessages.sh
+++ StaticMessages.sh
@@ -22,6 +22,30 @@ function import_po_files # First parameter will be a path that will contain seve
 	# https://github.com/python-babel/babel/issues/566
 	find "$podir" -name '*.po' -exec msgattrib --no-obsolete -o {} {} \;
 	ANSI_COLORS_DISABLED=1 a2po import --ignore-fuzzy --android res/ --gettext $podir
-}
 
+	# Generate the locales_config.xml
+	pushd res
+	echo '<?xml version="1.0" encoding="utf-8"?>' > xml/locales_config.xml
+	echo '<locale-config xmlns:android="http://schemas.android.com/apk/res/android">' >> xml/locales_config.xml
+	transform_locale_regex='(\w+)-r(\w+)'
+	# Add en-US as the first locale so that is the fallback, and also because it won't be handled in the following loop
+	echo -e '\t<locale android:name="en-US"/>' >> xml/locales_config.xml
+	for i in values-*; do
+		if [ -d "${i}" ]; then
+			if [ -e "${i}/strings.xml" ]; then
+				locale="${i:7}"
+				if [[ "${locale}" =~ $transform_locale_regex ]]; then
+					# Special case to turn locales like "en-rUS", "en-rGB" into "en-US" and "en-GB"
+					transformed_locale="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}"
+					echo -e "\t<locale android:name=\"${transformed_locale}\"/>" >> xml/locales_config.xml
+				else
+					echo -e "\t<locale android:name=\"${locale}\"/>" >> xml/locales_config.xml
+				fi
+			fi
+		fi
+	done
+
+	echo "</locale-config>" >> xml/locales_config.xml
+	popd
+}
 
