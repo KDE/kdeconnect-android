d114ff4c1c13519286c9f6ae277ff8aa54568b01
Albert Vaca Cintora <albertvaka@gmail.com>
2023-04-20 18:55:44 +0200

    Use getDefault to get the SSLContext

--- src/org/kde/kdeconnect/Helpers/SecurityHelpers/SslHelper.java
+++ src/org/kde/kdeconnect/Helpers/SecurityHelpers/SslHelper.java
@@ -209,7 +209,7 @@ public class SslHelper {
             trustManagerFactory.init(keyStore);
 
             // Setup custom trust manager if device not trusted
-            SSLContext tlsContext = SSLContext.getInstance("TLS");
+            SSLContext tlsContext = SSLContext.getDefault();
             if (isDeviceTrusted) {
                 tlsContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), RandomHelper.secureRandom);
             } else {
