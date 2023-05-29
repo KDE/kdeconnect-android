14270ea71a8ce072970557c7a54e66847ccc2298
Albert Vaca Cintora <albertvaka@gmail.com>
2023-05-29 15:02:58 +0200

    Re-add file removed by mistake

---
+++ src/org/kde/kdeconnect/Plugins/RunCommandPlugin/RunCommandWidgetDataProviderService.java
@@ -0,0 +1,12 @@
+package org.kde.kdeconnect.Plugins.RunCommandPlugin;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class RunCommandWidgetDataProviderService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RunCommandWidgetDataProvider(this, intent);
+    }
+}
