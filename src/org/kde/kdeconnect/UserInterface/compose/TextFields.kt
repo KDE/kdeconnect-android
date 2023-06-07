62918f065ef976cc65826f37f46ed9738bebb878
Aniketh <aniketh5660@gmail.com>
2023-06-07 20:39:29 +0530

    TextField adjustment

--- src/org/kde/kdeconnect/UserInterface/compose/TextFields.kt
+++ src/org/kde/kdeconnect/UserInterface/compose/TextFields.kt
@@ -8,20 +8,19 @@ package org.kde.kdeconnect.UserInterface.compose
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.kde.kdeconnect_tp.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KdeTextField(modifier: Modifier = Modifier, input: MutableState<String>, label: String) {
-    Column() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         var value by rememberSaveable { input
         }
         OutlinedTextField(value = value,
