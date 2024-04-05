5ce0c1ccf85cbb3ae40f1ab6bdc079ba09ef22b5
ShellWen Chen <me@shellwen.com>
2024-04-06 06:25:19 +0800

    refactor: migrate `AppsHelper` to Kotlin
    Remove `appIconLookup()` as not used

---
+++ src/org/kde/kdeconnect/Helpers/AppsHelper.kt
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2014 Albert Vaca Cintora <albertvaka@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+package org.kde.kdeconnect.Helpers
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+
+object AppsHelper {
+    @JvmStatic
+    fun appNameLookup(context: Context, packageName: String): String? {
+        return try {
+            val manager = context.packageManager
+            val info = manager.getApplicationInfo(packageName, 0)
+
+            manager.getApplicationLabel(info).toString()
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e("AppsHelper", "Could not resolve name $packageName", e)
+            null
+        }
+    }
+}
