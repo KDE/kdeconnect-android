551b089d9ae4602df5d251bbe63ccb9de4146748
Luca Carlon <carlon.luca@gmail.com>
2024-10-08 15:48:23 +0000

    Do not use FLAG_MUTABLE when targeting Android >= 34
    BUG: 494158

--- src/org/kde/kdeconnect/Plugins/SharePlugin/ReceiveNotification.java
+++ src/org/kde/kdeconnect/Plugins/SharePlugin/ReceiveNotification.java
@@ -160,7 +160,7 @@ class ReceiveNotification {
                 device.getContext(),
                 0,
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
         );
 
         builder.setContentText(device.getContext().getResources().getString(R.string.received_file_text, filename))
@@ -169,7 +169,7 @@ class ReceiveNotification {
         shareIntent = Intent.createChooser(shareIntent,
                 device.getContext().getString(R.string.share_received_file, destinationUri.getLastPathSegment()));
         PendingIntent sharePendingIntent = PendingIntent.getActivity(device.getContext(), (int) System.currentTimeMillis(),
-                shareIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+                shareIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         NotificationCompat.Action.Builder shareAction = new NotificationCompat.Action.Builder(
                 R.drawable.ic_share_white, device.getContext().getString(R.string.share), sharePendingIntent);
         builder.addAction(shareAction.build());
