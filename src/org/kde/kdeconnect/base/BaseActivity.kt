b567a90b060d4eea24261e2214a8e9f63110d7c0
Mash Kyrielight <fiepi@live.com>
2024-12-23 18:54:03 +0800

    Refactor: Move window insets handling to BaseActivity
    This change moves the logic for handling window insets to `BaseActivity`.
    It introduces a new function `onWindowInsetsChanged` to handle the insets and updates the padding of the decor view accordingly.
    This allows subclasses to customize the insets handling by overriding `onWindowInsetsChanged`.
    Additionally, a new property `isListActivity` is added to `BaseActivity` to control whether the bottom padding should be applied for list activities.

--- src/org/kde/kdeconnect/base/BaseActivity.kt
+++ src/org/kde/kdeconnect/base/BaseActivity.kt
@@ -7,26 +7,35 @@ package org.kde.kdeconnect.base
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import org.kde.kdeconnect.extensions.getSafeDrawInsets
 import org.kde.kdeconnect.extensions.setOnApplyWindowInsetsListenerCompat
 
 abstract class BaseActivity : AppCompatActivity() {
 
+    open val isListActivity = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-            window.decorView.setOnApplyWindowInsetsListenerCompat { v, insets ->
-                val safeDrawInsets = insets.getSafeDrawInsets()
-                v.updatePadding(
-                    top = 0,
-                    bottom = safeDrawInsets.bottom,
-                    left = safeDrawInsets.left,
-                    right = safeDrawInsets.right
-                )
+            window.decorView.setOnApplyWindowInsetsListenerCompat { decorView, insets ->
+                onWindowInsetsChanged(decorView, insets)
                 insets
             }
         }
     }
+
+    open fun onWindowInsetsChanged(decorView: View, insets: WindowInsetsCompat) {
+        val safeDrawInsets = insets.getSafeDrawInsets()
+        decorView.updatePadding(
+            top = 0,
+            bottom = if (isListActivity) 0 else safeDrawInsets.bottom,
+            left = safeDrawInsets.left,
+            right = safeDrawInsets.right
+        )
+    }
+
 }
