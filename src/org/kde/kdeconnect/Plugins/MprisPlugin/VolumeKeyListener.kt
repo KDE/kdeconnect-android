172822239cdd4789b6298d1b8e4da665d328dbd9
TPJ Schikhof <kde@schikhof.eu>
2024-12-29 10:54:01 +0000

    Migrate VolumeKeyListener to Kotlin

--- src/org/kde/kdeconnect/Plugins/MprisPlugin/VolumeKeyListener.kt
+++ src/org/kde/kdeconnect/Plugins/MprisPlugin/VolumeKeyListener.kt
@@ -3,14 +3,9 @@
  *
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
+package org.kde.kdeconnect.Plugins.MprisPlugin
 
-
-package org.kde.kdeconnect.Plugins.MprisPlugin;
-
-public interface VolumeKeyListener {
-
-    void onVolumeUp();
-
-    void onVolumeDown();
-
+interface VolumeKeyListener {
+    fun onVolumeUp()
+    fun onVolumeDown()
 }
