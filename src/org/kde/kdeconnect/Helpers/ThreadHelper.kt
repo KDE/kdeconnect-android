7081f81df991c25e3f5a5a7fd21b40cf8cb572ce
Albert Vaca Cintora <albertvaka@gmail.com>
2025-10-14 15:51:59 +0200

    Add helper methods to assert we run on the main thread or not

--- src/org/kde/kdeconnect/Helpers/ThreadHelper.kt
+++ src/org/kde/kdeconnect/Helpers/ThreadHelper.kt
@@ -1,11 +1,13 @@
 /*
- * SPDX-FileCopyrightText: 2022 Wojciech Matuszewski <combaine@gmail.com>
+ * SPDX-FileCopyrightText: 2025 Albert Vaca Cintora <albertvaka@gmail.com>
  *
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
 
 package org.kde.kdeconnect.Helpers
 
+import android.os.Looper
+import org.kde.kdeconnect_tp.BuildConfig
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -15,4 +17,20 @@ object ThreadHelper {
 
     @JvmStatic
     fun execute(command: Runnable) = executor.execute(command)
+
+    fun assertMainThread() {
+        if (BuildConfig.DEBUG) {
+            if (Thread.currentThread() == Looper.getMainLooper().thread) {
+                throw IllegalStateException("This function must be called from the Main thread.")
+            }
+        }
+    }
+
+    fun assertNotMainThread() {
+        if (BuildConfig.DEBUG) {
+            if (Thread.currentThread() != Looper.getMainLooper().thread) {
+                throw IllegalStateException("This function must be called from a background thread.")
+            }
+        }
+    }
 }
