2782d0b0becb5f65575ac8b17325f24fca2cf98d
Albert Vaca Cintora <albertvaka@gmail.com>
2026-09-12 08:55:20 +0200

    Release 1.35.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "org.kde.kdeconnect_tp"
         minSdk = 23
         targetSdk = 37
-        versionCode = 13515
-        versionName = "1.35.15"
+        versionCode = 13516
+        versionName = "1.35.16"
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
     buildFeatures {
